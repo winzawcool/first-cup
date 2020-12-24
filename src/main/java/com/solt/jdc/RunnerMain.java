84b2fe87cb2fd7c9359ccee2991f9a42d378e26d
Win Zaw Oo <iwinzawcool@gmail.com>
2020-12-24 10:38:29 +0630

    wzo second commit

--- src/main/java/com/solt/jdc/RunnerMain.java
+++ src/main/java/com/solt/jdc/RunnerMain.java
@@ -1,5 +1,8 @@
+package com.solt.jdc;
+
 public class RunnerMain {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("second commit");
     }
 }
